9025a1186d052cb9cf22bfeb0b30f6337359c516
Byron Lloyd-Wakeman <byzstorm1103@gmail.com>
2021-04-24 07:44:35 -0800

    Adds FindAsyncDeterminant and some helper methods
    Added FindAsyncDeterminant which causes unexpected errors and needs to be debugged

--- ConsoleForTesting/Program.cs
+++ ConsoleForTesting/Program.cs
@@ -2,24 +2,24 @@
 using MatrixLibraryTests;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace ConsoleForTesting
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Matrix matrix = new Matrix(3, 3);
+            Matrix matrix = new Matrix(8, 8);
 
-            matrix.EditMatrix(0, 0, 1);
-            matrix.EditMatrix(1, 0, 3);
-            matrix.EditMatrix(2, 0, 1);
-            matrix.EditMatrix(0, 1, 0);
-            matrix.EditMatrix(1, 1, 4);
-            matrix.EditMatrix(2, 1, 1);
-            matrix.EditMatrix(0, 2, 2);
-            matrix.EditMatrix(1, 2, -1);
-            matrix.EditMatrix(2, 2, 0);
+            for (int i = 0; i < matrix.NumberOfColumns; i++)
+            {
+                for (int j = 0; j < matrix.NumberOfRows; j++)
+                {
+                    matrix.EditMatrix(i, j, 1);
+                }
+            }
 
             Console.WriteLine(matrix.ToString());
 
@@ -28,9 +28,17 @@ namespace ConsoleForTesting
             //    Console.WriteLine(matrix.FindMatrixMinor(i, 0));
             //}
 
+            var watch = Stopwatch.StartNew();
             Console.WriteLine(matrix.FindDeterminant());
+            //Console.WriteLine(matrix.FindInverse());
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds);
 
-            Console.WriteLine(matrix.FindInverse());
+            //var watch2 = Stopwatch.StartNew();
+            //Console.WriteLine(await matrix.FindAsyncDeterminant());
+            ////Console.WriteLine(matrix.FindInverse());
+            //watch2.Stop();
+            //Console.WriteLine(watch2.ElapsedMilliseconds);
 
             Console.ReadKey();
         }
