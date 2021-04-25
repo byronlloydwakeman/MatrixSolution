37369a5553a4b16c668d68b50af91f138b909434
Byron Lloyd-Wakeman <byzstorm1103@gmail.com>
2021-04-25 09:24:11 -0800

    Added MatrixTransformationFactory
    Added a Transformation Factory for Matricies which includes the Methods
    ReflectionInXAxis and ReflectionInYAxis

---
+++ MatrixLibrary/MatrixTransformationFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixLibrary
+{
+    public class MatrixTransformationFactory
+    {
+        public static Matrix ReflectionInYAxis()
+        {
+            Matrix ReturnMatrix = MatrixFactory.IdentityMatrix(2);
+            ReturnMatrix.EditMatrix(0, 0, -1);
+
+            return ReturnMatrix;
+        }
+
+        public static Matrix ReflectionInXAxis()
+        {
+            Matrix ReturnMatrix = MatrixFactory.IdentityMatrix(2);
+            ReturnMatrix.EditMatrix(1, 1, -1);
+
+            return ReturnMatrix;
+        }
+    }
+}
